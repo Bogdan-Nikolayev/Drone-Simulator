d36b85c1fbfe3652ab2f5b6fa954188edd2cabbd
Bodix <bodix321@gmail.com>
2021-08-18 21:08:23 +0300

    Add project files

---
+++ Drone Simulator/Drone Simulator/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Drone_Simulator
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly SocketManager socketManager = new SocketManager();
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Clicked(object sender, EventArgs e)
+        {
+            string result = socketManager.SocketSendReceive("10.5.1.157", 10000);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
