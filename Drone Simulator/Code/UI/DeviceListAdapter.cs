8e3b0bcb0fe3b3854831ef162287c2579eee0815
Bodix <bodix321@gmail.com>
2021-09-01 19:04:35 +0300

    Add user interface

---
+++ Drone Simulator/Code/UI/DeviceListAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Net.Wifi.P2p;
+using Android.Views;
+using Android.Widget;
+
+namespace Drone_Simulator.UI
+{
+    public class DeviceListAdapter : BaseAdapter<WifiP2pDevice>
+    {
+        private readonly Activity _context;
+        private readonly List<WifiP2pDevice> _devices;
+
+        public DeviceListAdapter(Activity context, List<WifiP2pDevice> devices)
+        {
+            _context = context;
+            _devices = devices;
+        }
+
+        public override int Count => _devices.Count;
+
+        public override WifiP2pDevice this[int position] => _devices[position];
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View? GetView(int position, View? convertView, ViewGroup? parent)
+        {
+            View view = convertView ?? _context.LayoutInflater.Inflate(Resource.Layout.row_devices, null);
+
+            WifiP2pDevice device = _devices[position];
+
+            view.FindViewById<TextView>(Resource.Id.device_name).Text = device.DeviceName;
+            view.FindViewById<TextView>(Resource.Id.device_details).Text = device.Status.ToString();
+
+            return view;
+        }
+    }
+}
--- Drone Simulator/Code/UI/DeviceListFragment.cs
+++ Drone Simulator/Code/UI/DeviceListFragment.cs
@@ -1,15 +1,38 @@
-﻿using Android.OS;
+﻿using System.Collections.Generic;
+using Android.Net.Wifi.P2p;
+using Android.OS;
 using Android.Support.V4.App;
 using Android.Views;
+using Drone_Simulator.WifiDirect;
 
 namespace Drone_Simulator.UI
 {
     public class DeviceListFragment : ListFragment
     {
+        private readonly List<WifiP2pDevice> devices = new List<WifiP2pDevice>();
+
+        public DeviceListFragment()
+        {
+            Listener = new WifiDirectPeerListListener(Fill);
+        }
+
+        public WifiDirectPeerListListener Listener { get; }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            ListAdapter = new DeviceListAdapter(Activity, devices);
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // view = inflater.Inflate(Android.Resource.Layout.Dev)
-            return null;
+            return inflater.Inflate(Resource.Layout.fragment_device_list, null);
+        }
+
+        private void Fill(WifiP2pDeviceList peers)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
