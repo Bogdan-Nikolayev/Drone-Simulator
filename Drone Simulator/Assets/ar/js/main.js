c578c2f836d4048f1a4b4f683c16b21fa8f87906
Bodix <bodix321@gmail.com>
2021-09-06 18:34:40 +0300

    Improve JS logic

--- Drone Simulator/Assets/ar/js/main.js
+++ Drone Simulator/Assets/ar/js/main.js
@@ -1,6 +1,6 @@
-let showError = function (error) {
+function showError(error) {
   alert(error.name + ": " + error.message);
-};
+}
 
 // https://stackoverflow.com/a/27725393
 function escapeJson(string) {
