e757433cc5e043a830cff22c0499ccbca2223aa4
Bodix <bodix321@gmail.com>
2021-09-07 21:52:44 +0300

    Fix sending multiple messages at the same time

--- Drone Simulator/Code/Sockets/Socket.cs
+++ Drone Simulator/Code/Sockets/Socket.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Text;
+﻿using System.Text;
 using Android.OS;
 using Java.IO;
 using Java.Lang;
@@ -9,24 +8,19 @@ namespace Drone_Simulator.Sockets
     public abstract class Socket : ISocket
     {
         protected Java.Net.Socket _socket;
-        private readonly object _lockObject = new object();
-        private readonly object _lockObject2 = new object();
 
         public event SocketMessageHandler StringReceived;
 
         public void SendString(sbyte messageType, string message)
         {
-            lock (_lockObject)
-            {
-                Log.Debug("Start sending: " + message);
-                using DataOutputStream stream = new DataOutputStream(_socket.OutputStream);
-                byte[] bytes = Encoding.UTF8.GetBytes(message);
-                stream.WriteInt(bytes.Length + 1);
-                stream.WriteByte(messageType);
-                stream.Write(bytes);
+            using DataOutputStream stream = new DataOutputStream(_socket.OutputStream);
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            // 1 byte from "messageType".
+            stream.WriteInt(bytes.Length + 1);
+            stream.WriteByte(messageType);
+            stream.Write(bytes);
 
-                Log.Debug("Sent: " + message);
-            }
+            Log.Debug("Sent: " + message);
         }
 
 
@@ -37,78 +31,33 @@ namespace Drone_Simulator.Sockets
                 using DataInputStream inputStream = new DataInputStream(_socket.InputStream);
                 while (true)
                 {
-                    byte[] buffer = new byte[1024];
-                    int incomeLength = 0;
-                    int readLength = 0;
+                    int totalLength = inputStream.ReadInt();
+                    sbyte messageType = inputStream.ReadByte();
 
-                    while (incomeLength - readLength != 0)
-                        ReadMessage();
-
-                    void ReadMessage()
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    // 1 byte from "messageType".
+                    int progressLength = 1;
+                    while (progressLength != totalLength)
                     {
-                        int totalLength = inputStream.ReadInt();
-                        sbyte messageType = inputStream.ReadByte();
+                        byte[] buffer = new byte[1024];
+                        int remainingLength = totalLength - progressLength;
+                        int readLength = Math.Min(remainingLength, buffer.Length);
+                        inputStream.Read(buffer, 0, readLength);
+                        outputStream.Write(buffer, 0, readLength);
 
-                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-                        int progressLength = 1;
-                        Log.Debug("total: " + totalLength);
-                        while (progressLength != totalLength)
-                        {
-                            incomeLength = inputStream.Read(buffer);
-                            int remainingLength = totalLength - progressLength;
-                            readLength = Math.Min(remainingLength, buffer.Length);
-                            outputStream.Write(buffer, 0, readLength);
-
-                            progressLength += readLength;
-                            Log.Debug("income: " + incomeLength);
-                            Log.Debug("remain: " + remainingLength);
-                            Log.Debug("read: " + readLength);
-                            Log.Debug("progress: " + progressLength);
-                        }
-
-                        string message = Encoding.UTF8.GetString(outputStream.ToByteArray());
-
-                        // Run in UI thread.
-                        new Handler(Looper.MainLooper).Post(() => StringReceived?.Invoke(messageType, message));
-
-                        Log.Debug("Received: " + message);
+                        progressLength += readLength;
                     }
+
+                    string message = Encoding.UTF8.GetString(outputStream.ToByteArray());
+
+                    // Run in UI thread.
+                    new Handler(Looper.MainLooper).Post(() => StringReceived?.Invoke(messageType, message));
+
+                    Log.Debug("Received: " + message);
                 }
             }));
             thread.Start();
         }
-
-        // private void ReadMessage(DataInputStream inputStream)
-        // {
-        //     int totalLength = inputStream.ReadInt();
-        //     sbyte messageType = inputStream.ReadByte();
-        //
-        //     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        //
-        //     int progressLength = 1;
-        //     Log.Debug("total: " + totalLength);
-        //     while (progressLength != totalLength)
-        //     {
-        //         int incomeLength = inputStream.Read(buffer);
-        //         int remainingLength = totalLength - progressLength;
-        //         readLength = Math.Min(remainingLength, buffer.Length);
-        //         outputStream.Write(buffer, 0, readLength);
-        //
-        //         progressLength += readLength;
-        //         Log.Debug("income: " + incomeLength);
-        //         Log.Debug("remain: " + remainingLength);
-        //         Log.Debug("read: " + readLength);
-        //         Log.Debug("progress: " + progressLength);
-        //     }
-        //
-        //     string message = Encoding.UTF8.GetString(outputStream.ToByteArray());
-        //
-        //     // Run in UI thread.
-        //     new Handler(Looper.MainLooper).Post(() => StringReceived?.Invoke(messageType, message));
-        //
-        //     Log.Debug("Received: " + message);
-        // }
 
         public void Dispose()
         {
